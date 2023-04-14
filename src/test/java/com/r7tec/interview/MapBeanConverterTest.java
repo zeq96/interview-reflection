0985e5b899d037873f7eb741cce0be53903bac9e
andrew <zeq_96@163.com>
2023-04-14 11:08:27 +0800

    finish the test

---
+++ src/test/java/com/r7tec/interview/MapBeanConverterTest.java
@@ -0,0 +1,32 @@
+package com.r7tec.interview;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class MapBeanConverterTest {
+    @Test
+    public void test() {
+        MapBeanConverter.DemoJavaBean bean = new MapBeanConverter.DemoJavaBean();
+        bean.setId(100);
+        bean.setName("BBBBBBBBBBBBB");
+        Map<String, Object> resultMap = MapBeanConverter.beanToMap(bean);
+
+        Assertions.assertEquals(100, resultMap.get("id"));
+        Assertions.assertEquals("BBBBBBBBBBBBB", resultMap.get("name"));
+        Assertions.assertEquals(true, resultMap.get("longName"));
+        Assertions.assertNull(resultMap.get("olate"));
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("id", 456);
+        map.put("name", "12345");
+        MapBeanConverter.DemoJavaBean resultBean =
+                MapBeanConverter.mapToBean(MapBeanConverter.DemoJavaBean.class, map);
+
+        Assertions.assertEquals(456, resultBean.getId());
+        Assertions.assertEquals("12345", resultBean.getName());
+    }
+}
